a67f236c8b4ac38e23f4556d9bc70729ab2018c5
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-01-22 15:53:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 //your JS code here. If required.
-var currentUrl = window.location.href;
-alert("The length of the URL is: ", currentUrl.length);
+var currentUrl = window.location.href
+	
+ alert("The length of the URL is: " + currentUrl.length);
